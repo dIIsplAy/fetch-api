52db17113ed89fafe4c969e86639b5fecac6e9a1
cristophol <thomas.cristophol@gmail.com>
2017-08-03 11:29:12 +0200

    cmmit

--- new-article.php
+++ new-article.php
@@ -1,8 +1,9 @@
 <?php
-header ('Content-Type: text/plain');
-if (empty($_POST['title']) || empty($_POST['article'])) {
-http_response_code(400);
-echo 'missing required parameter';
-exit(1);
+header('Content-Type: text/plain');
+if (empty($_POST['title']) || empty($_POST['content'])) {
+    http_response_code(400);
+    echo 'missing required parameters';
+    exit(1);
 }
-echo $_POST['title'] . ' ' . $_POST['article'];
+echo $_POST['title'] . ' ' . $_POST['content'];
+exit(0);
